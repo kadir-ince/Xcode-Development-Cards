51618b33db873ebbf4127db1e7c79a09e0f7fe99
Kadir Ince <kadirincedev@gmail.com>
2020-07-17 18:01:13 +0300

    Initial Commit

---
+++ Xcode Development Cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode Development Cards
+//
+//  Created by Kadir Ince on 17.07.2020.
+//  Copyright © 2020 Kadir Ince. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
